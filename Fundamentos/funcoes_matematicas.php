a6407bdcbbe35be0f807e664d16f9788a922c35b
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2023-03-13 20:22:56 -0300

    Atualizando funções matemáticas

---
+++ Fundamentos/funcoes_matematicas.php
@@ -0,0 +1,7 @@
+<?php
+// Principais funções matemáticas em php https://www.php.net/manual/pt_BR/ref.math.php
+// Exemplos:
+echo abs(-45); //Numero absoluto;
+echo floor(5654.78); //Arredonda frações para baixo
+echo ceil(345.45); // Arredonda a fração para cima
+echo pow(10); //Eleva o numeral ao quadrado
