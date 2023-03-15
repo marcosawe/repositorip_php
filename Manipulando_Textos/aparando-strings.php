2a915378fff06b0344eb12c0a42a7694fbe1e083
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2023-03-15 12:23:04 -0300

    Modificando Strings

--- Manipulando_Textos/aparando-strings.php
+++ Manipulando_Textos/aparando-strings.php
@@ -2,6 +2,6 @@
 
 $csv = ',.Vinicius Dias,24,.';
 
-echo trim($csv, '.,') . PHP_EOL;
-echo ltrim($csv, '.,') . PHP_EOL;
-echo rtrim($csv, '.,') . PHP_EOL;
+echo trim($csv, '.,') . PHP_EOL; //trim — Retira espaço no início e final de uma string
+echo ltrim($csv, '.,') . PHP_EOL;//ltrim — Retira espaço no início de uma string
+echo rtrim($csv, '.,') . PHP_EOL;//rtrim — Retira espaço no final de uma string
